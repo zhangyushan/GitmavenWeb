edf4e168928ef0b4f126b0b931941cc0ecfcc563
lisi <lisi@126.com>
2017-08-24 14:34:26 +0800

    test

--- src/main/java/com/atguigu/git/bean/Student.java
+++ src/main/java/com/atguigu/git/bean/Student.java
@@ -3,6 +3,8 @@ package com.atguigu.git.bean;
 public class Student {
 	public static void main(String[] args) {
 		System.out.println("hello world...");
+		
+		System.out.println("too young to simple");
 	}
 
 }
